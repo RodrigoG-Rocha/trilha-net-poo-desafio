6b7834873b20810f90fd672f77a0ee19d81eb0f0
Rodrigo Rocha <rodrigogon.rochaofc@gmail.com>
2025-08-26 16:43:47 -0300

    Desafio Concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// IMPLEMENTADO
+class Program
+{
+    static void Main()
+    {
+        // IMPLEMENTADO
+
+        Smartphone nokia = new Nokia("188409", "Modelo 5", "1111111111", 20);
+        Smartphone iphone = new Iphone("497309", "Modelo 11", "2222222222", 62);
+
+        Console.WriteLine("\n--- Teste com Nokia ---");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("Snake");
+
+        Console.WriteLine("\n--- Teste com iPhone ---");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
